d587b1af1e1893464237d2ad0ee288dfaac261fe
byeongcheollim <lim6112j@gmail.com>
2025-05-18 10:54:07 +0900

    user input sum

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,5 @@
-let () = Printf.printf "%s\n" Hello.world
+open Stdio
+let () =
+  printf "Total: %F\n" (Hello.read_and_accumulate 0.)
+(* let () = Printf.printf "%s\n" Hello.world *)
 (* let () = print_endline "Hello, World!" *)
--- lib/hello.ml
+++ lib/hello.ml
@@ -1,1 +1,8 @@
-let world = "Hello From a module"
+open Base
+open Stdio
+let rec read_and_accumulate accum =
+  let line = In_channel.input_line In_channel.stdin in
+  match line with
+  | None -> accum
+  | Some x -> read_and_accumulate (accum +. Float.of_string x)
+
